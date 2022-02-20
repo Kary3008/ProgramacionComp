5a5e3c4cdc214dfc899ba5682ee5d0da3ae0a7d7
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-02-19 18:36:46 -0600

    Problem1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	//vector
+	vector<int> dias(n);
+	for(int i=0;i<n;i++){
+		cin>>dias[i]
+	}
+	
+	
+	int compra, vende, maximo = -1;
+	
+	for()
+	
+	cout<<maximo<<endl;
+	return 0;
+}
